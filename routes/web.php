920c0fbf112563282a25a30fa5506220660b0fb0
arifrahman.fauzi <arif.rahman1780@gmail.com>
2019-09-17 13:41:29 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hdtuto', function() {
+
+    $crawler = Goutte::request('GET', 'https://www.tokopedia.com/');
+
+    $crawler->filter('.blog-post-item h2 a')->each(function ($node) {
+
+      dump($node->text());
+
+    });
+
+});
