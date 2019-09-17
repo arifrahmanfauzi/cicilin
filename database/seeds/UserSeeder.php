d8cc720bb0363ad749dc69e95f0721778866b857
arifrahman.fauzi <arif.rahman1780@gmail.com>
2019-09-17 17:12:00 +0700

    User Seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create('id_ID');
+
+        for ($i=0; $i <=10 ; $i++) {
+          // code...
+          DB::table('users')->insert([
+            'name' => $faker->name,
+            'email' =>$faker->email,
+            'password' => bcrypt('kebersamaan');,
+            'api_token' => bcrypt('token'),
+          ]);
+        }
+    }
+}
