7fa8485c8cdc4b24350f6fcec5c1d93d9bd3304c
arifrahman.fauzi <arif.rahman1780@gmail.com>
2019-09-24 11:52:34 +0700

    nyoba scrapping tittle

--- app/Http/Controllers/ScrapperController.php
+++ app/Http/Controllers/ScrapperController.php
@@ -10,8 +10,11 @@ class ScrapperController extends Controller
     public function index(){
         $crawler = Goutte::request('GET', 'https://www.bukalapak.com/products?utf8=%E2%9C%93&source=navbar&from=omnisearch&search_source=omnisearch_organic&search%5Bhashtag%5D=&search%5Bkeywords%5D=sepatu');
         $crawler->filter('.product-description')->each(function ($node) {
-          print $node->filter('a')->text();
+          echo $node->filter('a')->text()."\r";
+          echo "<br>";
         });
+
+        //$crawler->filter('.product-description')->filter('a')->text();
 
         $data [] = null;
         // $crawler->filter('.product-description a')->each(function ($node) {
